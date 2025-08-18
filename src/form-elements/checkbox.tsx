d3b1c36b13a2a5fc4eb2bc4f47960a3b3de1793e
Sergey <badj93@mail.ru>
2025-08-18 09:59:08 +0300

    fix: fix checkbox state behavior

--- src/form-elements/checkbox.tsx
+++ src/form-elements/checkbox.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent, HTMLAttributes } from 'react';
+import { type ChangeEvent, type HTMLAttributes, useState } from 'react';
 import type { FormCraftField } from '../types.ts';
 
 interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
@@ -9,11 +9,26 @@ interface CheckboxProps extends HTMLAttributes<HTMLInputElement> {
 export function Checkbox({
   f: { name, placeholder, ...rest },
   onChange,
+  defaultChecked,
   ...props
 }: CheckboxProps) {
+  const [checked, setChecked] = useState(!!defaultChecked);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (typeof onChange === 'function') onChange(e);
+    setChecked(e.target.checked);
+  };
+
   return (
     <label htmlFor={name}>
-      <input id={name} onChange={onChange} name={name} {...rest} {...props} />
+      <input
+        id={name}
+        name={name}
+        {...rest}
+        {...props}
+        onChange={onChangeHandler}
+        defaultChecked={checked}
+      />
       <span>{placeholder}</span>
     </label>
   );
